f585dc3d592620bf4d8c892c48ee19b7a7ba7380
hatoo <hato2000@gmail.com>
2020-02-15 20:40:19 +0900

    save

--- kubeadm.rb
+++ kubeadm.rb
@@ -1,9 +1,14 @@
 execute "install docker" do
-    not_if "which docker"
-    user "root"
-    command "curl -fsSL https://get.docker.com | sh"
+  not_if "which docker"
+  user "root"
+  command "curl -fsSL https://get.docker.com | sh"
 end
 
 service "dphys-swapfile.service" do
-    action :disable
+  action :disable
+end
+
+["iptables", "arptables", "ebtables"].each do |package_name|
+  package package_name do
+  end
 end
