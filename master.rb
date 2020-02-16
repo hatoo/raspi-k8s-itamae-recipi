a5c8ca92977dda5610bd99cb78f31d1f3956c5f4
hatoo <hato2000@gmail.com>
2020-02-16 22:02:54 +0900

    save

--- master.rb
+++
@@ -1,5 +0,0 @@
-include_recipe "./kubeadm.rb"
-
-execute "hostnamectl set-hostname #{node["hostname"]}" do
-  not_if "test $(hostname) = #{node["hostname"]}"
-end
---
+++ node.rb
@@ -0,0 +1,16 @@
+include_recipe "./kubeadm.rb"
+
+execute "hostnamectl set-hostname #{node["hostname"]}" do
+  not_if "test $(hostname) = #{node["hostname"]}"
+end
+
+template "/etc/dhcpcd.conf" do
+  source "./dhcpcd.conf.erb"
+  variables(node: node)
+end
+
+execute "Add my host to /etc/hosts" do
+  not_if "grep #{node["hostname"]} /etc/hosts"
+  user "root"
+  command "echo 127.0.1.1 #{node["hostname"]} >> /etc/hosts"
+end
